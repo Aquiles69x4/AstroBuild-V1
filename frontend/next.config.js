daefa1e18054465e4364b9e6d36484c761f6db6b
Aquiles69x4 <xavir9123@gmail.com>
2025-09-18 13:58:40 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://backend-hazel-iota-16.vercel.app/',
   },
   images: {
     domains: [],
   },
 }
+
 
 module.exports = nextConfig
